8bbf99181bf3ac9bafbd1f13f76741d6f38de442
Shyam Lokhande <lokhandeshyam2002@gmail.com>
2021-09-10 14:17:28 +0530

    Projects Uploaded

---
+++ Reactjs_bootcamp/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Note from "../components/Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
